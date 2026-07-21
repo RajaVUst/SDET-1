415997b5af4394935cdc2a96cc13ea91618e904c
Jamesy Joseph <310926@ust.com>
2026-07-21 14:29:39 +0530

    Uncommented the original test

--- ui-tests/tests/edge-cases/empty_cart.spec.ts
+++ ui-tests/tests/edge-cases/empty_cart.spec.ts
@@ -1,11 +1,11 @@
-// import { test, expect } from '../../fixtures/fixtures';
-// import { logger } from '../../support/logger';
+import { test, expect } from '../../fixtures/fixtures';
+import { logger } from '../../support/logger';
 
-// test('Not able to Checkout With Empty Cart',async ({cartFlow, checkoutFlow, page}) => {
-//     await cartFlow.openCart();
-//     logger.info("Opened the Empty-Cart");
+test('Not able to Checkout With Empty Cart',async ({cartFlow, checkoutFlow, page}) => {
+    await cartFlow.openCart();
+    logger.info("Opened the Empty-Cart");
     
-//     await expect(page.getByRole('link', {name: /continue shopping/i})).toBeVisible();
-//     logger.info("Verifed No Checkout possible in Empty-Cart");
-//     }
-// );
+    await expect(page.getByRole('link', {name: /continue shopping/i})).toBeVisible();
+    logger.info("Verifed No Checkout possible in Empty-Cart");
+    }
+);
